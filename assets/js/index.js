68298e6010013e0682941cb3725ed17499a8c203
Cauã Vinicius Chaves Stupp <caua80190@gmail.com>
2022-11-15 02:13:25 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,16 @@
 const btn = document.querySelector('.switch-button');
+const body = document.querySelector('body');
+
 
 function handleClick() {
+
+    const styles = window.getComputedStyle(body, null).getPropertyValue('background-color');
+    const save = localStorage.setItem('background', styles);
+    
+
     if (btn) {
         btn.classList.toggle('active');
-        document.body.classList.toggle('active');
+        body.classList.toggle('active');
     }
 }
 
