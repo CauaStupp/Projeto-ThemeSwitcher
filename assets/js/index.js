33a256842fc652fc142c26b697628d3ce555d556
Cauã Vinicius Chaves Stupp <caua80190@gmail.com>
2022-11-20 20:24:13 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,43 @@
-const btn = document.querySelector('.switch-button');
-const body = document.querySelector('body');
+const body = document.querySelector("body");
+const btn = document.querySelector(".switch-button");
 
+
+window.onload = () => {
+
+}
+
+function saveValues(value, key) {
+    localStorage.setItem(value, key);
+}
+
+function getValues(value, key) {
+    const values = localStorage.getItem(value, key);
+    return values;
+}
 
 function handleClick() {
 
-    const styles = window.getComputedStyle(body, null).getPropertyValue('background-color');
-    const save = localStorage.setItem('background', styles);
-    
+    btn.classList.toggle('active');
+    body.classList.toggle('active');
 
-    if (btn) {
-        btn.classList.toggle('active');
-        body.classList.toggle('active');
+    const styleVerify = () => {
+        const styles = getComputedStyle(body, null).getPropertyValue('background-color');
+        return styles;
     }
+    
+    const btnVerify = () => {
+        
+        return btnAuth;
+    }
+
+    console.log(btnVerify());
+
+    // if (localStorage.getItem === undefined) {
+    //     saveValues('background', );
+    //     saveValues('buttonState');
+    // }
 }
+
 
 
 btn.addEventListener('click', handleClick);
